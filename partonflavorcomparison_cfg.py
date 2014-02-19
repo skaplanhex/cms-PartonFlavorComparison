ec5b5d66c1c7b618b86faaf54899c7035c76e958
Steven <skaplan@physics.rutgers.edu>
2014-02-19 11:53:09 -0600

    Added plots

--- partonflavorcomparison_cfg.py
+++ partonflavorcomparison_cfg.py
@@ -1,6 +1,27 @@
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('python')
+
+options.register('outfilename',
+                 "outfile.root",
+                 VarParsing.multiplicity.singleton,
+                 VarParsing.varType.string,
+                 "Name of output file"
+                 )
+options.register('useSubjets',
+                  False,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.bool,
+                  'Whether or not to analyze subjet based tagging'
+                  )
+## 'maxEvents' and 'outputFile' are already registered by the Framework, changing default value
+options.setDefault('maxEvents', 15)
+#options.setDefault('outputFile', 'jetmatchingplots.root')
+options.parseArguments()
+
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Demo")
+process = cms.Process("USER")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
@@ -9,12 +30,44 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        'file:ttbarevents_pythia6.root'
     )
 )
+process.myPartons = cms.EDProducer("PartonSelector",
+    src = cms.InputTag("genParticles"),
+    withLeptons = cms.bool(False)
+)
+process.selectedHadronsAndPartons = cms.EDProducer('HadronAndPartonSelector',
+           src = cms.InputTag("generator"),
+           particles = cms.InputTag("genParticles"),
+           partonMode = cms.string("Auto")
+   )
+process.flavourByRefAK5 = cms.EDProducer("JetPartonMatcher",
+ jets = cms.InputTag("ak5GenJets"),
+ coneSizeToAssociate = cms.double(0.3),
+ partons = cms.InputTag("myPartons")
+ )
+process.jetFlavourInfosAK5 = cms.EDProducer("JetFlavourClustering",
+      jets                     = cms.InputTag("ak5GenJets"),
+      bHadrons                 = cms.InputTag("selectedHadronsAndPartons","bHadrons"),
+      cHadrons                 = cms.InputTag("selectedHadronsAndPartons","cHadrons"),
+      partons                  = cms.InputTag("selectedHadronsAndPartons","partons"),
+      jetAlgorithm             = cms.string("AntiKt"),
+      rParam                   = cms.double(0.5),
+      ghostRescaling           = cms.double(1e-18),
+      hadronFlavourHasPriority = cms.bool(True)
+      )
 
-process.demo = cms.EDAnalyzer('PartonFlavorComparison'
+
+process.TFileService = cms.Service("TFileService",
+      fileName = cms.string(options.outfilename)
+)
+process.analyzerAK5 = cms.EDAnalyzer('PartonFlavorComparison',
+         particleSource = cms.InputTag("genParticles"),
+         jets = cms.InputTag("ak5GenJets"),
+         jetFlavourByRef = cms.InputTag("flavourByRefAK5"),
+         jetFlavourInfos = cms.InputTag("jetFlavourInfosAK5")
 )
 
 
-process.p = cms.Path(process.demo)
+process.p = cms.Path((process.myPartons+process.selectedHadronsAndPartons)*(process.flavourByRefAK5+process.jetFlavourInfosAK5)*process.analyzerAK5)
--- src/PartonFlavorComparison.cc
+++ src/PartonFlavorComparison.cc
@@ -29,6 +29,44 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/Common/interface/View.h"
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/getRef.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
+#include "DataFormats/JetReco/interface/Jet.h"
+#include "DataFormats/JetReco/interface/JetCollection.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/JetReco/interface/JetFloatAssociation.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/Math/interface/deltaR.h"
+
+#include "SimDataFormats/JetMatching/interface/JetFlavour.h"
+#include "SimDataFormats/JetMatching/interface/JetFlavourMatching.h"
+#include "SimDataFormats/JetMatching/interface/MatchedPartons.h"
+#include "SimDataFormats/JetMatching/interface/JetMatchedPartons.h"
+#include "SimDataFormats/JetMatching/interface/JetFlavourInfo.h"
+#include "SimDataFormats/JetMatching/interface/JetFlavourInfoMatching.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+//GenParticleCollection
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+
+#include "TH2D.h"
+#include "TH3D.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+using namespace edm;
+using namespace std;
 //
 // class declaration
 //
@@ -50,8 +88,31 @@ class PartonFlavorComparison : public edm::EDAnalyzer {
       virtual void endRun(edm::Run const&, edm::EventSetup const&);
       virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
       virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      virtual const char* partonFlavourToChar(int);
 
       // ----------member data ---------------------------
+      edm::Service<TFileService> fs;
+      edm::InputTag particles_,jets_,flavorByDR_,flavorByClustering_;
+      edm::Handle< reco::GenParticleCollection > particles;
+      edm::Handle< edm::View< reco::Jet > > jets;
+      edm::Handle< reco::JetMatchedPartonsCollection > flavorByDR;
+      edm::Handle< reco::JetFlavourInfoMatchingCollection > flavorByClustering;
+
+      //plots
+      TH1D* hPartonFlavorOld;
+      TH1D* hPartonFlavorNew;
+
+      TH2D* hPartonFlavorOld_Phi;
+      TH2D* hPartonFlavorNew_Phi;
+      TH2D* hPartonFlavorOld_Eta;
+      TH2D* hPartonFlavorNew_Eta;
+      TH2D* hPartonFlavorOld_Pt;
+      TH2D* hPartonFlavorNew_Pt;
+
+
+      TH1D* hJetPt;
+      TH1D* hJetEta;
+      TH1D* hJetPhi;
 };
 
 //
@@ -69,6 +130,10 @@ PartonFlavorComparison::PartonFlavorComparison(const edm::ParameterSet& iConfig)
 
 {
    //now do what ever initialization is needed
+  particles_ = iConfig.getParameter<InputTag>("particleSource");
+  jets_ = iConfig.getParameter<InputTag>("jets");
+  flavorByDR_ = iConfig.getParameter<InputTag>("jetFlavourByRef");
+  flavorByClustering_ = iConfig.getParameter<InputTag>("jetFlavourInfos");
 
 }
 
@@ -85,24 +150,87 @@ PartonFlavorComparison::~PartonFlavorComparison()
 //
 // member functions
 //
-
+const char*
+PartonFlavorComparison::partonFlavourToChar(int temp)
+{
+    int flav = abs(temp);
+    if (flav == 1){
+        return "d";
+    }
+    else if (flav == 2){
+        return "u";
+    }
+    else if (flav == 3){
+        return "s";
+    }
+    else if(flav == 4){
+        return "c";
+    }
+    else if (flav == 5){
+        return "b";
+    }
+    else if (flav == 21){
+        return "g";
+    }
+    else if (flav == 0){
+        return "no match";
+    }
+    else{
+        throw cms::Exception("PDGID Issue") << "PDGID Not recognized, investigate!";
+    }
+}
 // ------------ method called for each event  ------------
 void
 PartonFlavorComparison::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+    unsigned int lumi = iEvent.luminosityBlock();
+    unsigned int run = iEvent.run();
+    unsigned int event = iEvent.id().event();
+    
+    stringstream ss;
+    ss << run << ":" << lumi << ":" << event;
+    string eventAddress = ss.str();
+
+    iEvent.getByLabel(particles_,particles);
+    iEvent.getByLabel(jets_, jets);
+    iEvent.getByLabel(flavorByDR_,flavorByDR);
+    iEvent.getByLabel(flavorByClustering_, flavorByClustering );
+
+    for (reco::JetFlavourInfoMatchingCollection::const_iterator iMatch = flavorByClustering->begin(); iMatch != flavorByClustering->end(); ++iMatch) {
+      
+      int currentIndex = iMatch - flavorByClustering->begin();
+      //check to see if jets between collections match
+      if( edm::Ptr<reco::Candidate>((*iMatch).first.id(), (*iMatch).first.get(), (*iMatch).first.key())  != 
+        edm::Ptr<reco::Candidate>((*flavorByDR)[currentIndex].first.id(), (*flavorByDR)[currentIndex].first.get(), (*flavorByDR)[currentIndex].first.key()) ) continue;
+
+      const reco::Jet* iJet  = (*iMatch).first.get();
+      int oldPartonFlavor = 0;
+      const reco::GenParticleRef partonRef = (*flavorByDR)[currentIndex].second.algoDefinitionParton();
+      if ( partonRef.isNonnull() ) oldPartonFlavor = partonRef.get()->pdgId();
+      int newPartonFlavor = (*iMatch).second.getPartonFlavour();
+
+      double jetPt = iJet->pt();
+      double jetEta = iJet->eta();
+      double jetPhi = iJet->phi();
+
+      hJetPt->Fill(jetPt);
+      hJetEta->Fill(jetEta);
+      hJetPhi->Fill(jetPhi);
+
+      hPartonFlavorOld_Phi->Fill( jetPhi, partonFlavourToChar(oldPartonFlavor), 1 );
+      hPartonFlavorNew_Phi->Fill( jetPhi, partonFlavourToChar(newPartonFlavor), 1 );
+
+      hPartonFlavorOld_Eta->Fill( jetEta, partonFlavourToChar(oldPartonFlavor), 1 );
+      hPartonFlavorNew_Eta->Fill( jetEta, partonFlavourToChar(newPartonFlavor), 1 );
+
+      hPartonFlavorOld_Pt->Fill( jetPt, partonFlavourToChar(oldPartonFlavor), 1 );
+      hPartonFlavorNew_Pt->Fill( jetPt, partonFlavourToChar(newPartonFlavor), 1 );
+
+
+    }
 
 
 
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
 }
 
 
@@ -110,12 +238,24 @@ PartonFlavorComparison::analyze(const edm::Event& iEvent, const edm::EventSetup&
 void 
 PartonFlavorComparison::beginJob()
 {
+
+  hPartonFlavorOld_Phi = fs->make<TH2D>("hPartonFlavorOld_Phi", "Parton Flavor vs. Phi",150,0,3.141593,7,0,7);
+  hPartonFlavorNew_Phi = fs->make<TH2D>("hPartonFlavorNew_Phi", "Parton Flavor vs. Phi",150,0,3.141593,7,0,7);
+
+  hPartonFlavorOld_Eta = fs->make<TH2D>("hPartonFlavorOld_Eta", "Parton Flavor vs. Eta",600,-6,6,7,0,7);
+  hPartonFlavorNew_Eta = fs->make<TH2D>("hPartonFlavorNew_Eta", "Parton Flavor vs. Eta",600,-6,6,7,0,7);
+
+  hPartonFlavorOld_Pt = fs->make<TH2D>("hPartonFlavorOld_Pt", "Parton Flavor vs. Pt",500,0,500,7,0,7);
+  hPartonFlavorNew_Pt = fs->make<TH2D>("hPartonFlavorNew_Pt", "Parton Flavor vs. Pt",500,0,500,7,0,7);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
-PartonFlavorComparison::endJob() 
+PartonFlavorComparison::endJob()
 {
+  hPartonFlavorOld = hPartonFlavorOld_Pt->ProjectionY();
+  hPartonFlavorNew = hPartonFlavorNew_Pt->ProjectionY();
+
 }
 
 // ------------ method called when starting to processes a run  ------------
